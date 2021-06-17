012645f015194b6021f34a2754ebcb45ea221ec3
Michael-Siri <magicurtains@gmail.com>
2021-06-17 19:34:11 -0400

    update on text options and victory screen

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ function playRound(computer, player){
 
 }
 
-function resetScore(){
-    playerScore = 0;
-    computerScore = 0;
-}
+//Assigning functions to buttons
 
 const btn = document.querySelectorAll('#btn');
 btn.forEach((btn) =>{
@@ -65,9 +62,10 @@ btn.forEach((btn) =>{
                 setTimeout(toggleButtons, 0);
                 toggleBattleText();
                 battleText("Opponent was hit!");
-                setTimeout(toggleButtons, 2000);
-                setTimeout(toggleBattleText, 1700);
-
+                if(playerScore<5){
+                    setTimeout(toggleButtons, 2000);
+                    setTimeout(toggleBattleText, 1700);
+                }
             }
             else{
                 playClickSound()
@@ -78,23 +76,102 @@ btn.forEach((btn) =>{
                 setTimeout(toggleButtons, 0);
                 toggleBattleText();
                 battleText("You were hit!");
-                setTimeout(toggleButtons, 2000);
-                setTimeout(toggleBattleText, 1700);
+                if(computerScore <5){
+                    setTimeout(toggleButtons, 2000);
+                    setTimeout(toggleBattleText, 1700);
+                }
             }
         }
 
         if(playerScore>=5){
             console.log(`You win ${playerScore} to ${computerScore}`);
-            resetScore();
+            
+            var theme = document.getElementById("battleTheme");
+            theme.pause();
+
+            setTimeout(playVictoryTheme,2750);
+            setTimeout(endGameWin,3000);
+            setTimeout(endGameWin2,6000);
+            setTimeout(endGameWin3,9000);
+
             
         }
         else if (computerScore>=5){
             console.log(`You lose ${playerScore} to ${computerScore}`);
-            resetScore();
+            setTimeout(endGameLoss,3000);
+            setTimeout(endGameLoss2,6000);
+
         }
 
        });
 })
+
+const btn2 = document.getElementById("opponent");
+const btn3 = document.getElementById("you");
+
+btn2.addEventListener('click', () => {
+
+    setTimeout(toggleButtons, 0);
+    toggleBattleText();
+    
+    if(brockCounter < 3){
+        switch(brockCounter){
+            case 0: 
+                playClickSound()
+                battleText("BROCK: Don't touch me!");
+                brockCounter++;
+                break;
+            case 1:
+                playClickSound()
+                battleText("BROCK: I said stop touching me!");
+                brockCounter++;
+                break;
+            case 2:
+                playClickSound()
+                battleText("BROCK: I warned you! Get away!");
+                brockCounter++;
+                if(computerScore < 4){
+                    computerScore++;
+                    updateEnemyHealth(bar2,96-(19.2*computerScore));
+                }
+
+                break;
+        }
+
+    }
+    else{
+        battleText("Brock: ...");
+    }
+
+
+    setTimeout(toggleButtons, 2400);
+    setTimeout(toggleBattleText, 2200);
+
+
+});
+
+btn3.addEventListener('click', () => {
+
+    setTimeout(toggleButtons, 0);
+    toggleBattleText();
+
+    battleText("Denver, a bright and kind engineer graduate.");
+
+    setTimeout(toggleButtons, 2800);
+    setTimeout(toggleBattleText, 2600);
+});
+
+// btn2.addEventListener('click', () => {
+//     alert("hi");
+// });
+
+// btn3.addEventListener('click', () => {
+
+// });
+
+
+
+//Type message screen function
 
 var showText = function (target, message, index, interval) {   
     if (index < message.length) {
@@ -103,11 +180,11 @@ var showText = function (target, message, index, interval) {
     }
   }
 
-
+//Initial battleIntro
 
 function battleIntro(){
     $(function () {
-        showText("#intro", "Bandit has challenged you to a battle . . . ! ", 0, 50);   
+        showText("#intro", "A suspicious lurker has challenged you . . . ! ", 0, 50);   
       });
 }
 
@@ -115,7 +192,7 @@ function battleIntro(){
 function battleText(text){
     $( "#intro" ).empty();
     $(function () {
-        showText("#intro", text, 0, 50);   
+        showText("#intro", text, 0, 45);   
       });
 }
 
@@ -130,6 +207,11 @@ function playClickSound(){
     audio.play();
 };
 
+function playVictoryTheme(){
+    var audio = document.getElementById("victoryTheme");
+    audio.play();
+}
+
 
 function updateEnemyHealth(healthBar,value){
 healthBar.querySelector(".fill").style.width = `${value}%`;
@@ -137,7 +219,28 @@ healthBar.querySelector(".fill").style.width = `${value}%`;
 
 function toggleButtons(button1, button2, button3)
 {
-    $(".button1, .button2, .button3, .button4").toggle();
+    $(".button1, .button2, .button3, .button4, .invisible1, .invisible2").toggle();
+}
+
+//Endgame Text
+
+function endGameWin(){
+    battleText("BROCK: You won... I had a feeling about you...");
+}
+
+function endGameWin2(){
+    battleText("Congratulations! I knew you could do it!");
+} 
+
+function endGameWin3(){
+    battleText("Get well soon Denver,  Don't give up. -Mike");
+}
+
+function endGameLoss(){
+    battleText("BROCK: There is always another chance...");
+}
+function endGameLoss2(){
+    battleText("BROCK: You can win but only if you don't give up.");
 }
 
 const bar = document.querySelector(".progressBar");
@@ -153,6 +256,8 @@ setTimeout(battleIntro, 3000);
 setTimeout(toggleBattleText, 6750);
 
 setTimeout(toggleButtons, 7000);
+
+let brockCounter = 0;
 
 // End animations
 
